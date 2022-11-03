c518b2e02cef7ed39fe0a1630226ac81e5142eeb
epb90 <107769114+epb90@users.noreply.github.com>
2022-11-03 16:30:02 +0100

    Delete back end directory

--- back end/notes/operators.py
+++
@@ -1,4 +0,0 @@
-True is False
-True or False
-not True
-not False
--- back end/notes/ternary_operator.py
+++
@@ -1,6 +0,0 @@
-nice_weather = False
-going_outside = True if nice_weather else False
-print(going_outside)
-
-nice_weather_odds = .7
-party_location = 'inside' if nice_weather_odds < .6 else 'in the yard'
